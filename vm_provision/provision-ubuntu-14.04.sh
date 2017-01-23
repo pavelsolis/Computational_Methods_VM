929abfcd5fe54d94888b43af5f0ca5b9bdab5927
pavelsolis <pavel.solis@gmail.com>
2017-01-23 02:41:15 -0500

    Provision file changes
    Reduces the number of lines in the file

--- vm_provision/provision-ubuntu-14.04.sh
+++ vm_provision/provision-ubuntu-14.04.sh
@@ -1,24 +1,12 @@
 #!/usr/bin/env bash
 
-# Intended for Ubuntu 14.04 (Trusty)
-
-# Update Ubuntu
-apt-get update
-
-# Create the directories /sync and /tools under directory /usr
-echo "----- Provision: Creating directories for Comp Methods…”
-sudo su
-cd /
-cd usr/
-mkdir sync
-cd sync
-mkdir tools
+# Create the directory /usr/sync/tools
+echo "----- Provision: Creating directory for Comp Methods…”
+mkdir -p /usr/sync/tools
 
 # Create user accounts ‘setup’ and ‘self’
 echo "----- Provision: Creating user accounts for Comp Methods…”
-cd /
 adduser --disabled-password --gecos "" setup
 echo "setup:JHUEcon" | chpasswd
 adduser --disabled-password --gecos "" self
 echo "self:JHUEcon" | chpasswd
-exit
